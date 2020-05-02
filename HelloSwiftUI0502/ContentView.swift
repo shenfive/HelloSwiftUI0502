7a40d37223e646bd057c383ed2d53c06e55e8538
Danny Shen <shenfive@gmail.com>
2020-05-02 15:21:35 +0800

    Initial Commit

---
+++ HelloSwiftUI0502/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI0502
+//
+//  Created by 申潤五 on 2020/5/2.
+//  Copyright © 2020 申潤五. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
